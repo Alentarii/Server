cba7f69f639cb6b5abb8c868d5f00e257d4ec398
Alentarii <alentarii03@gmail.com>
2025-04-11 09:22:13 +0300

    Ready

--- Server.cpp
+++ Server.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <queue>
 #include <windows.h>
+#include <cassert>
 
 
 int main(int argc, char** argv) {
@@ -13,39 +14,52 @@ int main(int argc, char** argv) {
     void* receiver = zmq_socket(context, ZMQ_PULL);
     zmq_bind(receiver, "tcp://*:5050");
 
+    zmq_pollitem_t items[2];
+    /* First item refers to 0MQ socket 'socket' */
+    items[0].socket = receiver;
+    items[0].events = ZMQ_POLLIN;
+    /* Second item refers to standard socket 'fd' */
+    items[1].socket = NULL;
+    items[1].events = ZMQ_POLLIN;
+    /* Poll for events indefinitely */
+    int rc = zmq_poll(items, 1, 10000); //ожидаем 10 секунд
+    if (rc <= 0) {
+        std::cout << "Сообщений не получено";
+        return 0;
+    }
+
     std::queue<std::string> paths;
 
     int count = 0;
 
     for (;;) {
 
+        rc = zmq_poll(items, 1, 1000); //ожидаем 10 секунд
+        if (rc <= 0)
+            break;
+
         zmq_msg_t reply;
 
         char buffer[256];
         int size = zmq_recv(receiver, buffer, 255, 0);
+
         if (size == -1)
             return NULL;
         if (size > 255)
             size = 255;
         buffer[size] = '\0';
 
-        if (buffer[0] == '0')
-            count++;
-        else
-            paths.push(buffer);
-
-        if (count == 2)
-            break;        
+        paths.push(buffer);
 
         zmq_msg_close(&reply);
     }
 
     zmq_close(receiver);
 
-    
+
     void* sender = zmq_socket(context, ZMQ_PUSH);
     zmq_connect(sender, "tcp://localhost:4040"); //клиент 1
-    
+
     void* sender2 = zmq_socket(context, ZMQ_PUSH);
     zmq_connect(sender2, "tcp://localhost:6060"); //клиент 2
 
